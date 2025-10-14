c97d5d0b38a7363cbc43caf2ad20d4d767a07148
git status <215340863+g-sts@users.noreply.github.com>
2025-10-14 02:15:38 -0300

    feat: update UsuarioControler.java
    Add @GetMapping, add @DeleteMapping and add @PutMapping

--- src/main/java/com/meuprojeto/cadastro_usuario/controller/UsuarioController.java
+++ src/main/java/com/meuprojeto/cadastro_usuario/controller/UsuarioController.java
@@ -4,10 +4,7 @@ import com.meuprojeto.cadastro_usuario.business.UsuarioService;
 import com.meuprojeto.cadastro_usuario.infrastructure.entitys.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -22,5 +19,20 @@ public class UsuarioController {
         return ResponseEntity.ok().build();
     }
 
-    
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        usuarioService.deletarUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuariosPorEmail(@RequestParam String email, @RequestBody Usuario usuario){
+        usuarioService.atualizarUsuarioPorEmail(email, usuario);
+        return ResponseEntity.ok().build();
+    }
 }
